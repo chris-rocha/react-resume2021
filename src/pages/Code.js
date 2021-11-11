558601586cfeb1fcfc715bb9bf95b6e0d86ef950
Chris Rocha <chris@chrisrocha.com>
2021-11-11 13:33:20 -0500

    loop through code results

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react"
+import axios from "axios";
 
 export default class Code extends Component {
+    state = {
+        code: [],
+        loading: true
+    }
+    componentDidMount() {
+        axios.get(`https://z2t4tas4ok.execute-api.us-east-2.amazonaws.com/prod/code-examples`)
+          .then(res => {
+            const code = res.data.body;
+            this.setState({ code });
+            this.setState({loading: false});
+          })
+    }
     render() {
-        return(
-            <div className="inset">
-            {/* <div v-if="loading" className="loading"><span className="material-icons">autorenew</span></div> */}
-                <ul>
-                    <li>
-                        <a href="https://chrisrocha.com" target="_blank" rel="noreferrer">
-                            <img loading="lazy" src="" width="300" height="240" className="scale-with-grid" alt="" />
-                            <div className="caption">
-                                <h4>Site</h4>
-                                <div className="details">Details</div>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
+        if (this.state.loading === true) {
+            return (<div className="loading"><span className="material-icons">autorenew</span></div>)
+        } else {
+            return (
+                <div className="inset">
+                    <ul className="two">
+                        { this.state.code.map(code =>
+                            <li key={ code.slug }>
+                                <a href={ code.link } target="_blank" rel="noreferrer">
+                                    <img loading="lazy" src={ code.thumb } width="300" height="240" className="scale-with-grid" alt="" />
+                                    <div className="caption">
+                                        <h4>{ code.title }</h4>
+                                        <div className="details">{ code.details }</div>
+                                    </div>
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )
+        }
     }
 }
