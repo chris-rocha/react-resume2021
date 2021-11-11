09076b50e4f5063164dc946b0df1a7c45ce1719b
Chris Rocha <chris@chrisrocha.com>
2021-11-11 09:34:02 -0500

    add base page markup

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import './scss/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrap" id="app">
+        <header id="header" className="landing-max-width">
+            <div className="container">
+                <h1>Chris Rocha</h1>
+                <ul className="menu">
+                <li></li>
+                </ul>
+            </div>
+        </header>
+
+        <main id="content" role="main" className="landing-max-width">
+            <div className="container">
+            </div>
+        </main>
+
+        <footer id="footer" className="landing-max-width">
+            <div className="container">
+                <hr />
+                <ul className="footer-menu clearfix">
+                <li></li>
+                </ul>
+                <p className="legal">© currentYear chrisrocha.com</p>
+            </div>
+        </footer>
     </div>
-  );
+    );
 }
 
 export default App;
